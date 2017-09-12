730225d9119dd6f12a7e763d3c7596d9e8c8b5ca
Ben Brehaut <bencbrehaut@gmail.com>
2017-09-12 08:13:04 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,42 @@
-console.info('SW File');
+/**
+ * serive worker file
+ */
+
+const swVer = '1.0.0';
+
+const assetsCache = [
+    "./index.html",
+    "./stylesheets/app.css"
+]
 
 // Start Install Event
 self.addEventListener('install', function(event) {
-    // Wait until the promise is fulfiled.
-    event.waitUntil(
-        // Start caching files, within our serviceworker cache
-        caches.open('serviceworker').then(function(cache) {
-            console.log('Start cache');
-            // List of cache files
-            return cache.addAll([
-                "/index.html",
-                "/stylesheets/app.css"
-            ])
-        })
-    );
+  // Wait until the promise is fulfiled.
+  event.waitUntil(
+    // Start caching files, within our serviceworker cache
+    caches.open(swVer).then(function(cache) {
+      // List of cache files
+      return cache.addAll(assetsCache)
+    }).then(function() {
+      return self.skipWaiting();
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+	return self.clients.claim();
 });
 
 // Fetch our cache
 self.addEventListener('fetch', function(event) {
-    // Respond to the request
-    event.respondWith(
-        fetch(event.request).catch(function() {
-            // Return cache
-            return caches.match(event.request);
-        })
-    );
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
 });
